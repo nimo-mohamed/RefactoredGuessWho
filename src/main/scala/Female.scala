5d284e696728bbc669e28addf02ff48cf793f4ee
Farnoosh Moayeri <farnooshmoayeri@gmail.com>
2025-02-17 17:17:11 +0000

    Manual merging

--- src/main/scala/Female.scala
+++ src/main/scala/Female.scala
@@ -1,5 +1,7 @@
 package src.main.scala
 
-class Female (name: String, hairColour: Character.HairColour, eyeColour: Character.EyeColour) extends Character (name, hairColour, eyeColour) {
+
+class Female(name: String, hairColour: Character.HairColour, eyeColour: Character.EyeColour) extends Character("Female", name, hairColour, eyeColour) {
   override val describe: String = s"$name is a woman. She has ${hairColour.toString.toLowerCase} hair and ${eyeColour.toString.toLowerCase} eyes."
+
 }
