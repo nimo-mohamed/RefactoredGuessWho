505eab914629d33a60c39b4c73685fcaf44c9d68
Eloise Holland <eloise.holland@Mac.Home>
2025-02-17 10:59:01 +0000

    random character selection

--- src/main/scala/Female.scala
+++ src/main/scala/Female.scala
@@ -1,5 +1,5 @@
 package src.main.scala
 
 class Female (name: String, hairColour: Character.HairColour, eyeColour: Character.EyeColour) extends Character (name, hairColour, eyeColour) {
-  def describe: String = s"$name is a man. He has ${hairColour.toString.toLowerCase} hair and ${eyeColour.toString.toLowerCase} eyes."
+  val describe: String = s"$name is a woman. She has ${hairColour.toString.toLowerCase} hair and ${eyeColour.toString.toLowerCase} eyes."
 }
